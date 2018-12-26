48df52d31e75d06f88c71a2730bee78bb2f08f3b
Salahin <rocky.bgta@gmail.com>
2018-12-26 12:48:28 +0600

    add new seed data

--- src/main/java/response/soft/model/view/StoreInProductsViewModel.java
+++ src/main/java/response/soft/model/view/StoreInProductsViewModel.java
@@ -7,13 +7,18 @@ import java.util.UUID;
 
 @Data
 public class StoreInProductsViewModel {
-    private UUID vendorId;
+    private String storeName;
     private UUID storeId;
+    private String vendorName;
+    private UUID vendorId;
+    private String productName;
     private UUID productId;
     private String barcode;
-    private Integer quantity;
-    private Double unitPrice;
+    private String serialNo;
+    private String price;
+    private String quantity;
+    private String totalPrice;
     private Date entryDate;
-    private Date MFDate;
-    private Date EXPDate;
+    private Date manufacturingDate;
+    private Date expirationDate;
 }
