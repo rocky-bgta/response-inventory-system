9462b3cde14027d7cc0ca196523057b6df4a29d5
Salahin <rocky.bgta@gmail.com>
2019-01-05 19:30:18 +0600

    working product sales report view

---
+++ src/main/java/response/soft/controller/ProductSalesController.java
@@ -0,0 +1,81 @@
+package response.soft.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import response.soft.core.RequestMessage;
+import response.soft.core.ResponseMessage;
+import response.soft.model.StoreInProductModel;
+import response.soft.model.view.StoreInProductsViewModel;
+import response.soft.services.ProductSalesService;
+import response.soft.services.StockService;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/product-sales")
+@Api(tags = "Product Sales Api List")
+public class ProductSalesController {
+
+    @Autowired
+    private ProductSalesService productSalesService;
+
+    @Autowired
+    BeanFactory beanFactory;
+
+
+    @ApiOperation(value ="", response = Object.class)
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/report", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseMessage getAll(@RequestBody RequestMessage requestMessage,
+                                  @RequestParam(value="storeId", required = false) UUID storeId,
+                                  @RequestParam(value="productId", required = false) UUID productId){
+        ResponseMessage responseMessage;
+        responseMessage = this.productSalesService.getProductSalesReport(requestMessage);
+        return responseMessage;
+    }
+
+   /*
+
+    @ApiOperation(value ="", response = Object.class)
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseMessage getById(@PathVariable UUID id){
+        ResponseMessage responseMessage;
+        responseMessage = this.stockService.getByStockId(id);
+        return responseMessage;
+    }
+
+    @ApiOperation(value ="", response = StoreInProductsViewModel.class)
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseMessage save(@RequestBody RequestMessage requestMessage) {
+        ResponseMessage responseMessage;
+        responseMessage = this.stockService.saveStock(requestMessage);
+        return responseMessage;
+    }
+
+    @ApiOperation(value ="", response = StoreInProductModel.class)
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseMessage update(@RequestBody RequestMessage requestMessage) {
+        ResponseMessage responseMessage;
+        responseMessage = this.stockService.updateStock(requestMessage);
+        return responseMessage;
+    }
+
+    @ApiOperation(value ="", response = StoreInProductModel.class)
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "{id}",method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseMessage> delete(@PathVariable UUID id) {
+        ResponseMessage responseMessage;
+        responseMessage = this.stockService.deleteStock(id);
+        return new ResponseEntity(responseMessage,HttpStatus.OK);
+    }
+*/
+}
--- src/main/java/response/soft/services/ProductSalesService.java
+++ src/main/java/response/soft/services/ProductSalesService.java
@@ -1,21 +1,30 @@
 package response.soft.services;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import response.soft.appenum.SqlEnum;
 import response.soft.core.BaseService;
 import response.soft.core.Core;
+import response.soft.core.RequestMessage;
+import response.soft.core.ResponseMessage;
+import response.soft.core.datatable.model.DataTableRequest;
 import response.soft.entities.ProductSales;
 import response.soft.entities.StoreOutProduct;
+import response.soft.entities.view.AvailableStockView;
+import response.soft.entities.view.ProductSalesReportView;
 import response.soft.model.ProductSalesModel;
 import response.soft.model.StoreOutProductModel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ProductSalesService extends BaseService<ProductSales> {
-    private static final Logger log = LoggerFactory.getLogger(StoreInProductService.class);
+    private static final Logger log = LoggerFactory.getLogger(ProductSalesService.class);
 
     @Override
     protected void initEntityModel() {
@@ -25,16 +34,85 @@ public class ProductSalesService extends BaseService<ProductSales> {
         Core.runTimeModelType.set(ProductSalesModel.class);
     }
 
-    public void saveProductSales(List<StoreOutProductModel> storeOutProductModelList){
-        ProductSalesModel productSalesModel, savedProductSalesModel;
-        List<ProductSalesModel> savedProductSalesModelList = new ArrayList<>();
+    public ResponseMessage getProductSalesReport(RequestMessage requestMessage) {
+        ResponseMessage responseMessage;
+        List<ProductSalesReportView> list;
+        DataTableRequest dataTableRequest;
+        String searchKey=null;
+        StringBuilder queryBuilderString =new StringBuilder();
+        try {
+            Core.processRequestMessage(requestMessage);
 
-        for(StoreOutProductModel storeOutProductModel: storeOutProductModelList){
-            productSalesModel = new ProductSalesModel();
-            productSalesModel.setStoreOutId(storeOutProductModel.getId());
-            productSalesModel.setProductId(storeOutProductModel.getProductId());
+            dataTableRequest = requestMessage.dataTableRequest;
+            if(dataTableRequest!=null && !StringUtils.equals(dataTableRequest.search.value,"string")) {
+                searchKey = dataTableRequest.search.value;
+                searchKey = searchKey.trim().toLowerCase();
+            }
 
+            /*Set<ConstraintViolation<CountryModel>> violations = this.validator.validate(categoryModel);
+            for (ConstraintViolation<CountryModel> violation : violations) {
+                log.error(violation.getMessage());
+            }*/
+
+
+
+            //============ full text search ===========================================
+
+            if ((dataTableRequest != null && !StringUtils.isEmpty(searchKey))) {
+
+               /* queryBuilderString.append("SELECT v.id, ")
+                        .append("v.name, ")
+                        .append("v.phoneNo, ")
+                        .append("v.email, ")
+                        .append("v.address, ")
+                        .append("v.description ")
+                        .append("FROM Stock v ")
+                        .append("WHERE ")
+                        .append("( ")
+                        .append("lower(v.name) LIKE '%" + searchKey + "%' ")
+                        .append("OR lower(v.phoneNo) LIKE '%" + searchKey + "%' ")
+                        .append("OR lower(v.email) LIKE '%" + searchKey + "%' ")
+                        .append("OR lower(v.address) LIKE '%" + searchKey + "%' ")
+                        .append("OR lower(v.description) LIKE '%" + searchKey + "%' ")
+                        .append("OR lower(v.description) LIKE '%" + searchKey + "%' ")
+                        .append(") ")
+                        .append("AND v.status="+SqlEnum.Status.Active.get());
+
+                */
+
+
+                //Boolean isWhereAdded=false;
+                queryBuilderString.append("SELECT v FROM ProductSalesReportView v");
+
+
+
+
+                list = this.executeHqlQuery(queryBuilderString.toString(),ProductSalesReportView.class,SqlEnum.QueryType.View.get());
+                //============ full text search ===========================================
+            }else {
+                queryBuilderString.setLength(0);
+                queryBuilderString.append("SELECT v FROM ProductSalesReportView v");
+
+                list = this.executeHqlQuery(queryBuilderString.toString(),ProductSalesReportView.class,SqlEnum.QueryType.View.get());
+            }
+
+            responseMessage = this.buildResponseMessage(list);
+
+            if (list != null && list.size()>0) {
+                responseMessage.httpStatus = HttpStatus.FOUND.value();
+                responseMessage.message = "Get Product Sales Report successfully";
+                //this.commit();
+            } else {
+                responseMessage.httpStatus = HttpStatus.NOT_FOUND.value();
+                responseMessage.message = "Failed to get Product Sales Report";
+                //this.rollBack();
+            }
+        } catch (Exception ex) {
+            responseMessage = this.buildFailedResponseMessage();
+            ex.printStackTrace();
+            //this.rollBack();
+            log.error("getProductSalesReport -> save got exception");
         }
-
+        return responseMessage;
     }
 }
