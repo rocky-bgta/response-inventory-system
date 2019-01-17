c1e66b0440a294b46cadc45da178c754461eb2f8
Salahin <rocky.bgta@gmail.com>
2019-01-17 09:22:34 +0600

    re-new seed data

--- src/main/resources/db/changelog/seed/vendor.sql
+++ src/main/resources/db/changelog/seed/vendor.sql
@@ -1,2 +1,39 @@
-INSERT INTO inventory.vendor (id, name, phone_no, bank_ac_no, email, address, description, created_by, updated_by, created_date, updated_date, status) VALUES ('775b7b27-862b-478a-b6b2-df5f6fe7d66f', 'Kiam matal', '01254786', null, 'kiam@gmail.com', 'House No:- 64/1, Main Road Kallaynpur', 'All supplier', 'System', 'System', '2018-12-22 00:16:50.088000', '2018-12-22 00:16:50.088000', 1);
-INSERT INTO inventory.vendor (id, name, phone_no, bank_ac_no, email, address, description, created_by, updated_by, created_date, updated_date, status) VALUES ('1e28e828-1477-4f54-abdd-8d408b601ef3', 'BRB', '0125478556', null, 'brb.bangladesh@brb.com', 'Kushtia', 'ooo', 'System', 'System', '2018-12-22 00:19:42.191000', '2018-12-22 00:19:42.191000', 1);
+INSERT INTO "vendor" ("id", "name", "phone_no1", "phone_no2", "bank_info", "email", "address", "description", "created_by", "updated_by", "created_date", "updated_date", "status") VALUES ('029f7c4f-b5c4-4c61-a4cc-0773d24a744d', 'Excel technoliges ltd', '01958259112', NULL, 'datch bangla bank
+excel technoliges ltd
+A/c:-126.110.12491', NULL, 'elephant road,dhaka', 'digital x, X lab,excel speaker etc.', 'System', 'System', '2019-1-13 16:30:53.504', '2019-1-13 16:30:53.504', 1);
+INSERT INTO "vendor" ("id", "name", "phone_no1", "phone_no2", "bank_info", "email", "address", "description", "created_by", "updated_by", "created_date", "updated_date", "status") VALUES ('0c7cadec-45d7-4eea-a6c3-57a18b7b0f44', 'Hamim electronic', '01712791948', NULL, 'datch bangla bank 
+hamim electronic
+A/c:191.110.5246', NULL, 'bajoy nagor,dhaka', 'LED,CRT TV', 'System', 'System', '2019-1-13 16:12:16.51', '2019-1-13 16:12:16.51', 1);
+INSERT INTO "vendor" ("id", "name", "phone_no1", "phone_no2", "bank_info", "email", "address", "description", "created_by", "updated_by", "created_date", "updated_date", "status") VALUES ('1827d43d-8b09-4a02-989f-2db90124d82d', 'TM electronic', '01815493626', '01712081342', 'datch bangla bank
+amazon enterprice
+A/c:101.110.0043805', NULL, '144/a stadum market,dhaka
+', 'china led tv', 'System', 'System', '2019-1-16 20:54:36.167', '2019-1-16 21:02:16.345', 1);
+INSERT INTO "vendor" ("id", "name", "phone_no1", "phone_no2", "bank_info", "email", "address", "description", "created_by", "updated_by", "created_date", "updated_date", "status") VALUES ('42e63f63-2769-4050-88b1-a3e18348669b', 'Siko electronic', '01972229663', NULL, 'UCBL
+siko electronic
+A/c:-028 2101 000009542', NULL, 'nababpur,dhaka', 'LED TV,CRT TV', 'System', 'System', '2019-1-13 16:20:28.923', '2019-1-13 16:20:28.923', 1);
+INSERT INTO "vendor" ("id", "name", "phone_no1", "phone_no2", "bank_info", "email", "address", "description", "created_by", "updated_by", "created_date", "updated_date", "status") VALUES ('546fdba0-3f53-497f-8c70-0f59edd01ece', 'Respons electronic ', '01715363243', NULL, NULL, NULL, '1/3,lutfar munshe sorok,n.s road kushtia.', NULL, 'System', 'System', '2019-1-13 13:50:14.042', '2019-1-13 13:50:14.042', 1);
+INSERT INTO "vendor" ("id", "name", "phone_no1", "phone_no2", "bank_info", "email", "address", "description", "created_by", "updated_by", "created_date", "updated_date", "status") VALUES ('945d8216-1389-4b55-b905-902cb08a050c', 'T.M electronic', '01815493626', '01712081342', 'datch bangla bank
+amazon enterprise
+A/c:101.110.43805
+', NULL, 'stadium market,dhaka', ' all kind of china led tv', 'System', 'System', '2019-1-13 16:48:09.07', '2019-1-16 20:59:24', 3);
+INSERT INTO "vendor" ("id", "name", "phone_no1", "phone_no2", "bank_info", "email", "address", "description", "created_by", "updated_by", "created_date", "updated_date", "status") VALUES ('9c580c78-06cb-496c-afa6-adb0bcceb639', 'Khan & sons', '02-9551274', '01715084604', 'datch bangla bank
+MD.imran khan
+A/c:101.151.0033142', NULL, 'hoke stadium market,dhaka', ' all kind of china led tv', 'System', 'System', '2019-1-13 16:50:28.038', '2019-1-13 16:50:28.038', 1);
+INSERT INTO "vendor" ("id", "name", "phone_no1", "phone_no2", "bank_info", "email", "address", "description", "created_by", "updated_by", "created_date", "updated_date", "status") VALUES ('9d359626-e0db-4bd8-92ab-430f3e5e163a', 'Rafi computer', '01819220611', NULL, 'datch bangla bank
+rafi computer
+A/c:-126.110.8661', NULL, 'elephant road,dhaka', 'Classic brand', 'System', 'System', '2019-1-13 16:36:25.337', '2019-1-13 16:36:25.337', 1);
+INSERT INTO "vendor" ("id", "name", "phone_no1", "phone_no2", "bank_info", "email", "address", "description", "created_by", "updated_by", "created_date", "updated_date", "status") VALUES ('a708404e-5fc6-40f9-889a-da4c63259e99', 'Songata plus', '01712596860', NULL, 'datch bangla bank', NULL, 'nababpur,dhaka', 'LED TV,CRT TV', 'System', 'System', '2019-1-13 16:16:40.559', '2019-1-13 16:16:40.559', 1);
+INSERT INTO "vendor" ("id", "name", "phone_no1", "phone_no2", "bank_info", "email", "address", "description", "created_by", "updated_by", "created_date", "updated_date", "status") VALUES ('ab5b442d-0bb8-4542-b1dc-58b6a23b955f', 'Super star fan ltd', '01708459848', ' ', 'datch bangla bank
+Super star fan ltd
+A/c:105.110.0023310', NULL, 'sagun bagheca,dhaka', ' all kind of super star fan,roots lighting and etc', 'System', 'System', '2019-1-13 16:54:35.726', '2019-1-13 16:54:35.726', 1);
+INSERT INTO "vendor" ("id", "name", "phone_no1", "phone_no2", "bank_info", "email", "address", "description", "created_by", "updated_by", "created_date", "updated_date", "status") VALUES ('d51973b1-5c77-4aa6-9f0a-94fc2ad35e42', 'Maxwell trading corproration', '01819126509', '01819140484', 'UCBL
+Maxwell trading corproration
+A/c:107 2101 000004185
+', NULL, 'palton road,dhaka', 'micromax,micrologic,abs all kind of speaker', 'System', 'System', '2019-1-13 16:43:30.574', '2019-1-13 16:43:30.574', 1);
+INSERT INTO "vendor" ("id", "name", "phone_no1", "phone_no2", "bank_info", "email", "address", "description", "created_by", "updated_by", "created_date", "updated_date", "status") VALUES ('ddf23a46-4717-46d2-bbdf-f820831f44ef', 'zakir electronic', '01911720670', NULL, 'datch bangla bank
+M.G technoliges
+a/c:-101.110.0044971', NULL, 'staudiam market.dhaka', NULL, 'System', 'System', '2019-1-10 21:19:26.772', '2019-1-10 21:19:26.772', 1);
+INSERT INTO "vendor" ("id", "name", "phone_no1", "phone_no2", "bank_info", "email", "address", "description", "created_by", "updated_by", "created_date", "updated_date", "status") VALUES ('edcfbdcb-e149-40bc-8ae8-d2b0573913e5', 'Smart technoliges ltd', '01777734227', NULL, NULL, NULL, NULL, 'microlab,xtream speaker.', 'System', 'System', '2019-1-10 15:59:25.849', '2019-1-10 15:59:25.849', 1);
+INSERT INTO "vendor" ("id", "name", "phone_no1", "phone_no2", "bank_info", "email", "address", "description", "created_by", "updated_by", "created_date", "updated_date", "status") VALUES ('f8abf2a4-d48e-4f24-9863-e85346a6ca07', 'Solar electronic', '02-48321190', '01740103392', 'dutch bangla bank
+md.arif khan
+a/c:101.151.84268', 'solarelectrnics18@gmail.com', '48,west chowdhury para,ramna,dkaka-1219', 'solarvision led tv', 'System', 'System', '2019-1-16 21:41:23.017', '2019-1-16 21:41:23.017', 1);
