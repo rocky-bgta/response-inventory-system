14584a8d8a4531b4867285fb1d8eb5bfa6240ae4
Salahin <rocky.bgta@gmail.com>
2019-01-18 13:55:47 +0600

    create composite primary key in sales report view to solve duplicate row problem

--- src/main/java/response/soft/entities/view/ProductSalesReportView.java
+++ src/main/java/response/soft/entities/view/ProductSalesReportView.java
@@ -3,28 +3,29 @@ package response.soft.entities.view;
 import jdk.nashorn.internal.ir.annotations.Immutable;
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Date;
 import java.util.UUID;
 
 @Data
 @Entity
+@IdClass(ProductSalesReportView.class)
 @Immutable
 @Table(name = "product_sales_report_view")
-public class ProductSalesReportView {
+public class ProductSalesReportView implements Serializable {
+    @Id
+    @Column(name = "invoice_no")
+    private String invoiceNo;
+
     @Id
     @Column(name = "product_id")
     private UUID productId;
 
-    @Column(name = "invoice_no")
-    private String invoiceNo;
-
     @Column(name = "product_name")
     private String productName;
 
+    @Id
     @Column(name = "customer_id")
     private UUID customerId;
 
