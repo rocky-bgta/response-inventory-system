f91606a6b282f69771de114c240672655a2af082
Salahin <rocky.bgta@gmail.com>
2019-01-24 21:53:09 +0600

    working search in invoice

--- src/main/java/response/soft/services/InvoiceHistoryService.java
+++ src/main/java/response/soft/services/InvoiceHistoryService.java
@@ -10,10 +10,9 @@ import response.soft.core.BaseService;
 import response.soft.core.Core;
 import response.soft.core.RequestMessage;
 import response.soft.core.ResponseMessage;
-import response.soft.core.datatable.model.DataTableRequest;
 import response.soft.entities.InvoiceHistory;
+import response.soft.entities.view.InvoiceHistoryView;
 import response.soft.model.InvoiceHistoryModel;
-import response.soft.model.view.InvoiceHistoryViewModel;
 
 import java.util.List;
 import java.util.UUID;
@@ -32,11 +31,10 @@ public class InvoiceHistoryService extends BaseService<InvoiceHistory> {
 
     public ResponseMessage getInvoiceDetailsList(RequestMessage requestMessage, UUID customerId) {
         ResponseMessage responseMessage;
-        List<InvoiceHistoryViewModel> list=null;
+        List<InvoiceHistoryView> list;
         String searchKey;
         StringBuilder queryBuilderString =new StringBuilder();
         try {
-            this.resetPaginationVariable();
             Core.processRequestMessage(requestMessage);
             searchKey = Core.dataTableSearchKey.get();
 
@@ -48,73 +46,25 @@ public class InvoiceHistoryService extends BaseService<InvoiceHistory> {
             for (ConstraintViolation<CountryModel> violation : violations) {
                 log.error(violation.getMessage());
             }*/
-
-            queryBuilderString.append("SELECT invh.id AS invoiceId, ")
-                    .append("c.id AS customerId, ")
-                    .append("c.name AS customerName, ")
-                    .append("invh.invoiceNo, ")
-                    .append("invh.grandTotal AS invoiceAmount, ")
-                    .append("case cp.paidStatus " +
-                            " when 1 then 'Paid'" +
-                            " when 2 then 'Partial' " +
-                            " when 3 then 'Due' end AS invoiceStatus, ")
-                    .append("invh.date ")
-                    .append("FROM InvoiceHistory invh ")
-                    .append("INNER JOIN CustomerPayment cp ON invh.invoiceNo = cp.invoiceNo ")
-                    .append("INNER JOIN Customer c ON cp.customerId = c.id");
-
-
             //============ full text search ===========================================
 
             if ((searchKey != null && !StringUtils.isEmpty(searchKey))|| customerId!=null) {
 
-            /*    SELECT invh.id AS invoiceId,
-                c.id AS customerId,
-                c.name AS customerName,
+                queryBuilderString.append("SELECT v FROM InvoiceHistoryView v ")
+                        .append("WHERE ")
+                        //.append("( ")
+                        .append("lower(v.customerName) LIKE '%" + searchKey + "%' ")
+                        .append("OR lower(v.invoiceNo) LIKE '%" + searchKey + "%' ")
+                        .append("OR CAST(v.invoiceAmount AS string) LIKE '%" + searchKey + "%' ")
+                        .append("OR lower(v.invoiceStatus) LIKE '%" + searchKey + "%' ");
+                        //.append(") ");
 
-                invh.invoiceNo,
-                invh.grandTotal AS invoiceAmount,
-                case cp.paidStatus when 0 then 'Paid' when 2 then 'Partial' when 3 then 'Due' end AS invoiceStatus
-            FROM
-                InvoiceHistory invh
-                INNER JOIN CustomerPayment cp ON invh.invoiceNo = cp.invoiceNo
-                INNER JOIN Customer c ON cp.customerId = c.id*/
-
-
-
-
-                       /* .append("WHERE ")
-                        .append("( ")
-                        .append("lower(v.name) LIKE '%" + searchKey + "%' ")
-                        .append("OR lower(v.phoneNo) LIKE '%" + searchKey + "%' ")
-                        .append("OR lower(v.email) LIKE '%" + searchKey + "%' ")
-                        .append("OR lower(v.address) LIKE '%" + searchKey + "%' ")
-                        .append("OR lower(v.description) LIKE '%" + searchKey + "%' ")
-                        .append("OR lower(v.description) LIKE '%" + searchKey + "%' ")
-                        .append(") ");
-                */
-
-                        //.append("AND v.status="+SqlEnum.Status.Active.get());
-
-
-
-              /*  queryBuilderString.append("SELECT v FROM AvailableStockView v WHERE v.availableQty>0 ");
-                if(storeId!=null &&  storeId instanceof UUID){
-                    queryBuilderString.append("AND v.storeId ='"+storeId+"' ");
-                    //isWhereAdded=true;
-                }*/
-
-
-
-
-                //list = this.executeHqlQuery(queryBuilderString.toString(),AvailableStockView.class,SqlEnum.QueryType.View.get());
+                list = this.executeHqlQuery(queryBuilderString.toString(),InvoiceHistoryView.class,SqlEnum.QueryType.View.get());
                 //============ full text search ===========================================
             }else {
-                //queryBuilderString.setLength(0);
-                //queryBuilderString.append("SELECT v FROM AvailableStockView v WHERE v.availableQty>0");
-
-                list = this.executeHqlQuery(queryBuilderString.toString(),InvoiceHistoryViewModel.class,SqlEnum.QueryType.Join.get());
-
+                queryBuilderString.setLength(0);
+                queryBuilderString.append("SELECT v FROM InvoiceHistoryView v");
+                list = this.executeHqlQuery( queryBuilderString.toString(),InvoiceHistoryView.class,SqlEnum.QueryType.View.get());
             }
 
             responseMessage = this.buildResponseMessage(list);
