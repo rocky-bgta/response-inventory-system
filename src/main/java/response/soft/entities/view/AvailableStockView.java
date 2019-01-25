3a5250e8458893e8e541bc1591faff88a07d2044
Salahin <rocky.bgta@gmail.com>
2019-01-25 13:44:10 +0600

    modify view for available stock

--- src/main/java/response/soft/entities/view/AvailableStockView.java
+++ src/main/java/response/soft/entities/view/AvailableStockView.java
@@ -3,21 +3,25 @@ package response.soft.entities.view;
 import jdk.nashorn.internal.ir.annotations.Immutable;
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.UUID;
 
 @Data
 @Entity
+@IdClass(AvailableStockView.class)
 @Immutable
 @Table(name = "available_stock_view")
-public class AvailableStockView {
+public class AvailableStockView implements Serializable {
     @Id
     @Column(name = "product_id")
     private UUID productId;
 
+    @Id
+    @Column(name = "category_id")
+    private UUID categoryId;
+
+    @Id
     @Column(name = "store_id")
     private UUID storeId;
 
