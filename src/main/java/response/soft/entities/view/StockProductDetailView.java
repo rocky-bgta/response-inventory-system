bb3022514a6e1b551d75bd575fae3188457b0d79
Salahin <rocky.bgta@gmail.com>
2019-02-13 20:24:30 +0600

    create view

---
+++ src/main/java/response/soft/entities/view/StockProductDetailView.java
@@ -0,0 +1,66 @@
+package response.soft.entities.view;
+
+import jdk.nashorn.internal.ir.annotations.Immutable;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@Entity
+@Immutable
+@Table(name = "stock_product_details_view")
+public class StockProductDetailView implements Serializable {
+    @Id
+    @Column(name = "product_id")
+    private UUID productId;
+
+    @Id
+    @Column(name = "store_id")
+    private UUID storeId;
+
+    @Id
+    @Column(name = "vendor_id")
+    private UUID vendorId;
+
+    @Column(name = "vendor_name")
+    private UUID vendorName;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @Column(name = "category_id")
+    private String categoryId;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "brand_name")
+    private String brandName;
+
+    @Id
+    @Column(name = "model_no")
+    private String modelNo;
+
+    @Id
+    @Column(name = "stock_qty")
+    private Integer stockQty;
+
+    @Id
+    @Column(name = "buy_price")
+    private Double buyPrice;
+
+    @Id
+    @Column(name = "sale_price")
+    private Double salePrice;
+
+    @Id
+    @Column(name = "entry_date")
+    private Date entryDate;
+
+}
